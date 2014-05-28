36082f1ed7a086766bf38f5c5f72cb183aee6eaf
TeamRory <rorycteam@gmail.com>
2014-05-28 14:05:53 -0400

    Initial commit

--- server.rb
+++ server.rb
@@ -1,22 +1,16 @@
 require 'sinatra'
-require 'csv'
+require 'pg'
 require 'pry'
 
-def movies_array
-  movies =[]
-  CSV.foreach('movies.csv', headers: true) do |row|
-    movie = {
-      id: row["id"],
-      title: row["title"],
-      year: row["year"],
-      synopsis: row["synopsis"],
-      rating: row["rating"],
-      genre: row["genre"],
-      studio: row["studio"]
-    }
-    movies << movie
+def db_connection
+  begin
+    connection = PG.connect(dbname: 'movies')
+
+    yield(connection)
+
+  ensure
+    connection.close
   end
-  movies
 end
 
 def get_20_movies (page_number, all_movies)
@@ -25,7 +19,8 @@ def get_20_movies (page_number, all_movies)
 end
 
 get '/' do
-  redirect '/movies'
+
+  erb :index
 end
 
 get '/movies' do
@@ -35,29 +30,50 @@ get '/movies' do
   else
     @return_page = @page_number.to_i - 1
   end
-  @movies = get_20_movies(@page_number.to_i, movies_array)
+  movies = db_connection do |conn|
+    conn.exec('SELECT title, id FROM movies;')
+  end
+  @movies = get_20_movies(@page_number.to_i, movies.to_a)
   @page_number = @page_number.to_i + 1
 
-  erb :movies
+  erb :'movies/index'
 end
 
 get '/:title/:id' do
-  @movies = movies_array
+  movies = db_connection do |conn|
+    conn.exec('SELECT movies.title, movies.id, movies.year, movies.rating, genres.name AS genre, studios.name AS studio, movies.synopsis FROM movies LEFT OUTER JOIN genres ON genres.id = movies.genre_id JOIN studios ON studios.id = movies.studio_id;')
+    end
+  actors = db_connection do |conn|
+    conn.exec('SELECT movies.title, actors.name AS actor, cast_members.character FROM movies JOIN actors ON movies.actor_id = actors.id JOIN cast_members ON actors.id = cast_members.actor_id;')
+    end
+  @actors = actors.to_a
+  @movies = movies.to_a
   @movies.find do |each_hash|
     each_hash[:title] == params[:title]
     each_hash[:id] == params[:id]
   end
-  erb :id
+
+  erb :'movies/show'
+end
+
+get 'actors' do
+    @page_number = params[:page] || 1
+  if @page_number.to_i <= 1
+    @return_page = 1
+  else
+    @return_page = @page_number.to_i - 1
+  end
+  movies = db_connection do |conn|
+    conn.exec('SELECT title, id FROM movies;')
+  end
+  @movies = get_20_movies(@page_number.to_i, movies.to_a)
+  @page_number = @page_number.to_i + 1
+
+  erb :'movies/index'
 end
 
 get '/search' do
-  movie_hash = movies_array
-  @movies = []
-   movie_hash.each do |movie|
-    if movie[:title].downcase.include? params['query'].downcase
-     @movies << movie
-    end
-  end
+
   erb :search
 end
 
