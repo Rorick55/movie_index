c20fd5d10a92681ef7ad3604c74e979e40f8b75d
TeamRory <rorycteam@gmail.com>
2014-05-29 08:58:33 -0400

    The search bar is officially functional. With this commit I add the search function for both actors names and movie titles using postgresql querys. Along the way I realized a lot of my movies were not displaying in the show file due to not having a studio in the database. I fixed this with a simple outer join to include movies even if they don't have a studio id in the movies table. Another quick fix was names that had special characters in them. I changed the url to only include actors id. This stopped the names from messing with the params hash while linking to their show file.

--- server.rb
+++ server.rb
@@ -41,11 +41,11 @@ get '/movies' do
   erb :'movies/index'
 end
 
-get '/movies/:title/:id' do
+get '/movies/:id' do
   movies = db_connection do |conn|
     conn.exec("SELECT movies.title, movies.id, movies.year, movies.rating, genres.name AS genre,
       studios.name AS studio, movies.synopsis FROM movies LEFT OUTER JOIN genres ON genres.id = movies.genre_id
-      JOIN studios ON studios.id = movies.studio_id WHERE movies.id = '#{params[:id]}';")
+      LEFT OUTER JOIN studios ON studios.id = movies.studio_id WHERE movies.id = '#{params[:id]}';")
     end
   movie_actors = db_connection do |conn|
     conn.exec("SELECT movies.title, actors.id, actors.name AS actor, cast_members.character
@@ -55,8 +55,8 @@ get '/movies/:title/:id' do
   @actors = movie_actors.to_a
   @movies = movies.to_a
   @movies.find do |each_hash|
-    each_hash['title'] == params[:title]
-    each_hash['id'] == params[:id]
+    each_hash[:title] == params[:title]
+    each_hash[:id] == params[:id]
   end
 
   erb :'movies/show'
@@ -78,23 +78,50 @@ get '/actors' do
   erb :'actors/index'
 end
 
-get '/actors/:name/:id' do
+get '/actors/:id' do
   actors = db_connection do |conn|
-      conn.exec("SELECT actors.name, actors.id, movies.title, movies.id AS movie_id, cast_members.character
-        FROM cast_members JOIN actors ON actors.id = cast_members.actor_id JOIN movies
-        ON movies.id = cast_members.movie_id WHERE actors.id = '#{params[:id]}';")
-      end
+    conn.exec("SELECT actors.name, actors.id, movies.title, movies.id AS movie_id, cast_members.character
+      FROM cast_members JOIN actors ON actors.id = cast_members.actor_id JOIN movies
+      ON movies.id = cast_members.movie_id WHERE actors.id = '#{params[:id]}';")
+  end
 
     @actors = actors.to_a
     @actors.find do |each_hash|
-      each_hash['name'] == params[:name]
       each_hash['id'] == params[:id]
     end
   erb :'actors/show'
   end
 
-get '/search' do
-
-  erb :search
+get '/search/movies' do
+  @page_number = params[:page] || 1
+  if @page_number.to_i <= 1
+    @return_page = 1
+  else
+    @return_page = @page_number.to_i - 1
+  end
+    search_movies = params[:query_m]
+  movies = db_connection do |conn|
+    conn.exec("SELECT title, id FROM movies WHERE title ILIKE '%#{search_movies}%' ORDER BY title;")
+  end
+    @movies = get_20_movies(@page_number.to_i, movies.to_a)
+    @page_number = @page_number.to_i + 1
+  erb :'movies/search'
 end
+
+get '/search/actors' do
+  @page_number = params[:page] || 1
+  if @page_number.to_i <= 1
+    @return_page = 1
+  else
+    @return_page = @page_number.to_i - 1
+  end
+    search_actors = params[:query_a]
+  actors = db_connection do |conn|
+    conn.exec("SELECT name, id FROM actors WHERE name ILIKE '%#{search_actors}%' ORDER BY name;")
+  end
+    @actors = get_20_movies(@page_number.to_i, actors.to_a)
+    @page_number = @page_number.to_i + 1
+  erb :'actors/search'
+end
+
 
